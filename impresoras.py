b97f1a27faae083571f8a911ad32039a193f9389
anticristo19892 <161092815+anticristo19892@users.noreply.github.com>
2024-04-10 18:48:03 -0500

    actividad de desempeño 2

---
+++ impresoras.py
@@ -0,0 +1,42 @@
+print("***el punto de la impresora ***")
+vlimp= 100000
+iva = 1.27
+
+print("favor ingresar el valor de impresorasque desea comprar")
+cant = float(input())
+while cant <0:
+   print("la cantidad debe ser mayor a 0")
+   cant = float(imput())
+   pass
+
+print("ingrese su pago asi: 1 contado- 2 tarjeta de credito- 3 bono de reagalo")
+fpago: float = float(input())
+print("ERROR AL INGRESAR SU TIPO DE PAGO")
+print(" ingrese su valor de pago asi: 1contado- 2 tarjeta de credito- 3 bono de reagalo")
+fpago = float(imput())
+while fpago >=4:
+  print("ERROR AL INGRESAR SU TIPO DE PAGO")
+print("ingrese su valor de pago asi: 1contado- 2 tarjeta de credito- 3 bono de reagalo")
+fpago = int(imput())
+pass
+print(f"El descuento es:{esc:.of}")
+print(f"su total a pagar es de:{ totpagar:of}")
+case2
+vlunit = vlinmp * 1.27
+vltot = (vlimp * cant) * 1.27
+desc = vltot *.05
+totpagar = (vlimp - desc)
+print(f"la cantidad de impresoras que compro son:{cant}")
+print(f"el valor unitario de la impresora es:{vlunt}")
+print(f"el descuento es:{desc:.of}")
+print(f"su total a pagar es de:{totpagar:of}")
+
+case3
+vlunit = vlinmp * 1.27
+vltot = (vlimp * cant) * 1.27
+desc = vltot *.05
+totpagar = (vlimp - desc)
+print(f"la cantidad de impresoras que compro son:{cant}")
+print(f"el valor unitario de la impresora es:{vlunt}")
+print(f"el descuento es:{desc:.of}")
+print(f"su total a pagar es de:{totpagar:of}")
