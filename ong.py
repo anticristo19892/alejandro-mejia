32f71af4ec1815b0c5bae26b776113ecfc385729
anticristo19892 <161092815+anticristo19892@users.noreply.github.com>
2024-11-11 22:15:54 -0500

    Delete ong.py

--- ong.py
+++
@@ -1,6 +0,0 @@
-vacunas_disponibles = 1000
-para cada dia
-cantidad_entregada = solicitar_cantidad_entregada()  # Supongamos que hay una función para esto
-vacunas_disponibles = vacunas_disponibles - cantidad_entregada
-si vacunas_disponibles < 200 entonces
-mostrar "¡Alerta! El inventario de vacunas es menor a 200 unidades."
