0113ef7dc52d8dbc835eeb563ea33ba28dd3810b
anticristo19892 <161092815+anticristo19892@users.noreply.github.com>
2024-11-11 22:13:39 -0500

    Delete actividad de conocimiento factorial 1 .py

--- actividad de conocimiento factorial 1 .py
+++
@@ -1,8 +0,0 @@
-def calcular_factorial(n):
-    factorial = 1
-    for i in range(1, n + 1):
-        factorial *= i
-    return factorial
-numero = 5
-factorial_numero = calcular_factorial(numero)
-print(f"El factorial de {numero} es: {factorial_numero}")
