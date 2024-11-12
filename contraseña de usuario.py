269860d1dc7b27942e9493ce723c8f3cb8111595
anticristo19892 <161092815+anticristo19892@users.noreply.github.com>
2024-11-11 22:14:11 -0500

    Delete contraseña de usuario.py

--- contraseña de usuario.py
+++
@@ -1,8 +0,0 @@
-contrasena_correcta = "p217"
-while True:
-    contrasena = input("Ingrese la contraseña: ")
-    if contrasena == contrasena_correcta:
-        print("Bienvenido")
-        break  # Salir del bucle cuando se ingresa la contraseña correcta
-    else:
-        print("Contraseña incorrecta. Inténtelo nuevamente.")
