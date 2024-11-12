c846dbfac126c5530d48f1824c71f59cb255206f
anticristo19892 <161092815+anticristo19892@users.noreply.github.com>
2024-11-11 22:20:56 -0500

    Delete taller de sumas de numeros2.4.py

--- taller de sumas de numeros2.4.py
+++
@@ -1,7 +0,0 @@
-def suma_numeros_pares(lista_numeros):
-    suma_pares = sum(num for num in lista_numeros if num % 2 == 0)
-    return suma_pares
-entrada = input("1,2,3,4,: ")
-numeros = [int(num) for num in entrada.split(',')]
-suma_pares = suma_numeros_pares(numeros)
-print("Suma de los números pares:", suma_pares)
